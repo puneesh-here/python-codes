69f5f465058cf5519cceb21c6612fa7c615a5a6e
hardikhere <web.hardikhere@gmail.com>
2020-12-09 20:02:50 +0530

    loopelse and try

---
+++ loopelse.py
@@ -0,0 +1,10 @@
+a=int(input("Enter your level:"))
+b=int(input("Enter your level that you have to Go:"))
+i=a
+while i<=b:
+    c=(i+2)
+    print(c,'')
+    i+=1
+else:
+    print("")
+    print("You are in your next level")    
---
+++ try.py
@@ -0,0 +1,7 @@
+a= input("Enter Your:")
+if a == "hello":
+   print("Good Day,how can I help you..") 
+elif a == "good morning":
+   print("hi,good morning") 
+else:
+    print("sorry I can not understand..try again")   
