8603a28814c9b40efd4d0b95cc3acb9918973d66
hardikhere <web.hardikhere@gmail.com>
2020-12-19 18:26:07 +0530

    Def And Return in python...

---
+++ defandreturn.py
@@ -0,0 +1,13 @@
+a=input("enter your name:-")
+def hello (name):
+    print("hi,"+a+ "!...")
+hello("Puneesh")
+
+def Add (num1,num2):
+    print(num1+num2)
+Add(10,15)
+
+def returnAdd(num1,num2):
+    return(num1+num2)
+sum=returnAdd(6,6)
+print(sum)   
